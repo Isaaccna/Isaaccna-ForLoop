2d603ea9db828688a003b2eb5329723f92c302f2
Isaaccna <isaac.andrade1231@gmail.com>
2023-02-03 03:02:35 +0000

    done

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -39,10 +39,20 @@ public class Lab {
      *      @param end the value of i where the for loop should end (provided that the for loop is incremented with i++)
      *      @return the sum of all numbers between start and end, with start inclusive and end exclusive.
      */
+
+     int start;
+     int end;
+
     public String returnNumbers(int start, int end){
-        String buildingString = "";
+        
 
 //        code here
+int i = start;
+String buildingString = "";
+for (i = start; i < end; i++ ) {
+System.out.println(i);
+buildingString = buildingString + i + " ";
+}
 
         return buildingString;
     }
